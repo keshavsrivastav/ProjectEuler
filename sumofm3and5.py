6d3f6feef76eb790b2819dff2434f86f53de17d7
keshavsrivastav <keshav.sri@outlook.com>
2020-04-25 19:50:32 +0530

    First Commit

--- sumofm3and5.py
+++ sumofm3and5.py
@@ -1,9 +1,1 @@
-def isMultipleOf(number):
-    if number % 3 == 0:
-        return True
-    if number % 5 == 0:
-        return True
-    return False
-
-numbers = filter(isMultipleOf,range(1000))
-print(sum(numbers))
+print(sum([i for i in range(1000) if i%3==0 or i%5==0]))
